6502272ac6e8807f7015fd5c67643cb4e6fea7f3
Vlad Goia <vlad@sponsorit.live>
2022-02-25 13:03:17 +0100

    Added improved email scrapper

--- email_scrapper.py
+++ email_scrapper.py
@@ -16,7 +16,7 @@ def set_chrome_options() -> None:
     Chrome options for headless browser is enabled.
     """
     chrome_options = Options()
-    chrome_options.add_argument("headless")
+    # chrome_options.add_argument("headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-extensions");
@@ -28,7 +28,7 @@ def set_chrome_options() -> None:
     return chrome_options
 
 
-DRIVER_PATH = './chromedriver98'
+DRIVER_PATH = './chromedriver97'
 
 #For docker
 chrome_options = set_chrome_options();
@@ -40,7 +40,8 @@ def logInsta(driver):
     try:
         driver.get("https://www.instagram.com/accounts/login/")
         time.sleep(1)
-        cookiesButton = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div[role='dialog'] button")))[0]
+        cookiesButton = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div[role='dialog'] .aOOlW")))[0]
+        print(cookiesButton)
         ActionChains(driver).move_to_element(cookiesButton).click(cookiesButton).perform()
         time.sleep(1)
         user = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input[name='username']")))
@@ -59,53 +60,57 @@ def logInsta(driver):
 dataCollected = {}
 headerWritten= False
 loggedFirstLog = False
-with open('./twitch_users_networks_total.csv', newline='') as csvfile:
-    with open('twitch_users_emails.csv', 'w') as emailsFile:
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        if not headerWritten:
-            emailsFile.write("user,instagramEmail,twitterEmail,\n");
-            headerWritten = True
-        for row in spamreader:
-            
-            if not LogedIn:
-                print("Not logged in")
-                LogedIn = logInsta(driver)
-            if LogedIn:
-                if not loggedFirstLog:
-                    print("Logged In")
-                    loggedFirstLog = False
-                userName = row[0]
-                print(userName)
-                    if userName != "user":
-                        dataCollected[userName]={}
-                        emailsFile.write(userName+",")
+LogedIn=False
 
-                        if(row[2] is not ''):
-                            try:
-                                driver.get(row[2])
-                                descriptionSpans = WebDriverWait(driver,3).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "header section div:last-of-type span")))
-                                for span in descriptionSpans:
-                                    spanText = span.text
-                                    dataCollected[row[0]]["instagramBussinesEmail"]=re.findall(r'[\w.+-]+@[\w-]+\.[\w.-]+', spanText)
-                                if "instagramBussinesEmail" in dataCollected[row[0]]:
-                                    emailsFile.write("%s"%(dataCollected[row[0]]["instagramBussinesEmail"]))
-                                emailsFile.write(",")
-                            except:
-                                print("Invalid instagram link")
-                        if(row[1] is not ''):
-                            try:
-                                driver.get(row[1])
-                                descriptionSpans = WebDriverWait(driver,3).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div[data-testId='UserDescription'] span")))
-                                for span in descriptionSpans:
-                                    spanText = span.text
-                                    dataCollected[row[0]]["twitterBussinesEmail"]=re.findall(r'[\w.+-]+@[\w-]+\.[\w.-]+', spanText)
-                                if "twitterBussinesEmail" in dataCollected[row[0]]:
-                                    emailsFile.write("%s"%(dataCollected[row[0]]["twitterBussinesEmail"]))
-                                emailsFile.write(",")
-                            except:
-                                print("Invalid Twitter link")
+with open('./allNetworks25feb.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    
+    for row in spamreader:
+        
+        if not LogedIn:
+            print("Not logged in")
+            LogedIn = logInsta(driver)
+        if LogedIn:
+            if not loggedFirstLog:
+                print("Logged In")
+                loggedFirstLog = True
+            userName = row[0]
+            print(userName)
+            if userName != "user":
+                dataCollected[userName]={}
+                with open('./twitch_users_emails_data.csv', 'a') as emailsFile:
+                    if not headerWritten:
+                        emailsFile.write("user,instagramEmail,twitterEmail,\n");
+                        print("Headder written")
+                        headerWritten = True
+                    emailsFile.write(userName+",")
+                    
+                    if(row[2] is not ''):
+                        try:
+                            driver.get(row[2])
+                            descriptionSpans = WebDriverWait(driver,3).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "header section div:last-of-type span")))
+                            for span in descriptionSpans:
+                                spanText = span.text
+                                dataCollected[row[0]]["instagramBussinesEmail"]=re.findall(r'[\w.+-]+@[\w-]+\.[\w.-]+', spanText)
+                            if "instagramBussinesEmail" in dataCollected[row[0]]:
+                                emailsFile.write("%s"%(dataCollected[row[0]]["instagramBussinesEmail"]))
+                            emailsFile.write(",")
+                        except:
+                            print("Invalid instagram link")
+                    if(row[1] is not ''):
+                        try:
+                            driver.get(row[1])
+                            descriptionSpans = WebDriverWait(driver,3).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div[data-testId='UserDescription'] span")))
+                            for span in descriptionSpans:
+                                spanText = span.text
+                                dataCollected[row[0]]["twitterBussinesEmail"]=re.findall(r'[\w.+-]+@[\w-]+\.[\w.-]+', spanText)
+                            if "twitterBussinesEmail" in dataCollected[row[0]]:
+                                emailsFile.write("%s"%(dataCollected[row[0]]["twitterBussinesEmail"]))
+                            emailsFile.write(",")
+                        except:
+                            print("Invalid Twitter link")
 
-                        emailsFile.write("\n")
+                    emailsFile.write("\n")
                     
 driver.quit()
 
